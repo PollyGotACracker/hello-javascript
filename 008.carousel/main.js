3998764a2e0868eaf0f5bffb31c84e5182e2ef33
PollyGotACracker <bitterns96@gmail.com>
2023-05-15 23:56:18 +0900

    carousel

--- 008.carousel/main.js
+++ 008.carousel/main.js
@@ -1,4 +1,6 @@
 // https://www.youtube.com/watch?v=IEbaqI7F8vM
+// 처음, 마지막 이미지를 별도로 추가해야 transition 이 자연스러움
+
 const images = document.querySelectorAll("#slider > img");
 const sliderContainer = document.querySelector("#slider-container");
 const pointerContainer = document.querySelector("#pointer-container");
@@ -6,20 +8,20 @@ const slider = document.querySelector("#slider");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
-// 앞 뒤 이미지를 어떻게 해야 할까??
-[...images].forEach(() => {
+// generate pointer buttons
+[...images].forEach((_, idx) => {
+  if (idx === 0 || idx === images.length - 1) return false;
   const pointer = document.createElement("DIV");
   pointer.className = "pointer";
-  pointer.style.width = `${100 / images.length}%`;
+  pointer.dataset.num = idx - 1;
   pointerContainer.append(pointer);
 });
 
-let current = 1;
-let intervalSlide;
 // sliderContainer의 가로 길이
 let imgSize = sliderContainer.getBoundingClientRect().width;
+let current = 1;
 slider.style.transform = `translateX(${-imgSize}px)`;
-pointerContainer.children[0].classList.add("active");
+let intervalSlide;
 
 const prevImg = () => {
   if (current <= 0) return false;
@@ -45,19 +47,23 @@ const stopSlide = () => {
 };
 
 const changePointer = () => {
+  let num = current - 1;
+  // - (별도로 추가한 이미지 2장 + index 고려 1 )
+  if (current === 0) num = images.length - 3;
+  if (current === images.length - 1) num = 0;
   [...pointerContainer.children].forEach((pointer) => {
     pointer.classList.remove("active");
   });
-  pointerContainer.children[current].classList.add("active");
+  pointerContainer.children[num].classList.add("active");
 };
 
 const chkImg = () => {
-  if (images[current].classList.contains("first_img")) {
+  if (images[current] === slider.firstElementChild) {
     slider.style.transition = "none";
     current = images.length - 2;
     slider.style.transform = `translateX(${-imgSize * current}px)`;
   }
-  if (images[current].classList.contains("last_img")) {
+  if (images[current] === slider.lastElementChild) {
     slider.style.transition = "none";
     current = images.length - current;
     slider.style.transform = `translateX(${-imgSize * current}px)`;
@@ -71,20 +77,22 @@ const chkScreen = () => {
 
 const resizeImg = () => {
   stopSlide();
-  slider.style.transition = "none";
+  // container 의 사이즈가 브라우저 크기에 상대적일 경우, 새로운 값을 재할당
   imgSize = sliderContainer.getBoundingClientRect().width;
+  slider.style.transition = "none";
   slider.style.transform = `translateX(${-imgSize * current}px)`;
   startSlide();
 };
 
 const clickPointer = (idx) => {
-  current = idx;
+  current = idx + 1;
   slider.style.transform = `translateX(${-imgSize * current}px)`;
   slider.style.transition = "none";
   changePointer();
 };
 
 startSlide();
+changePointer();
 
 slider.addEventListener("transitionstart", changePointer);
 slider.addEventListener("transitionend", chkImg);
@@ -99,6 +107,7 @@ nextBtn.addEventListener("click", nextImg);
     clickPointer(idx);
   });
 });
+
 /*
   cf)
   transitionstart, transitionend :
